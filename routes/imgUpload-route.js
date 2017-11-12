fa59a216ce29b35df8ee870b8de28724fd9ff1a7
Pmookerjee <pmookerjee@gmail.com>
2017-11-12 13:35:08 -0800

    Added BUCKET from the .env file instead of literal string

--- routes/imgUpload-route.js
+++ routes/imgUpload-route.js
@@ -6,7 +6,7 @@ const aws = require('aws-sdk');
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const s3 = new aws.S3({ params: {Bucket: 'paulamookerjee'} });
+const s3 = new aws.S3({ params: {Bucket: process.env.AWS_BUCKET} });
 
 aws.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -17,7 +17,7 @@ const upload = multer({
 
    storage: multerS3({
        s3: s3,
-       bucket: 'paulamookerjee',
+       bucket: process.env.AWS_BUCKET,
        contentType: multerS3.AUTO_CONTENT_TYPE,       
        metadata: (req, file, cb ) => {
         cb(null, { fieldName: file.fieldname});
