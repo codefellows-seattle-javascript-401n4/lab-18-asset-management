799209574da25efb8415a608f8a63f5d7d140299
RyleeAndrews <ryleeandrews73@gmail.com>
2017-11-13 13:36:30 -0800

    everything all working now on to tests

--- routes/image-route.js
+++ routes/image-route.js
@@ -3,34 +3,35 @@
 const Image = require('../model/image.js');
 const express = require('express');
 const multer = require('multer');
-const httpErrors = require('http-errors');
-const s3 = require('../lib/s3.js');
+const fs = require('fs-extra');
+const aws = require('aws-sdk');
+const dotenv = require('dotenv').config();
+const s3 = new aws.S3();
 const bodyParser = require('body-parser').json();
 const multers3 = require('multer-s3');
 const imageRouter = module.exports = require('express').Router();
 
-const upload = multer({dest: `${__dirname}/../temp`})
+
 
 
 let accessKeyId = process.env.AWS_ACCESS_KEY_ID;
 let secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-
-AWS.config.update({
+aws.config.update({
   accessKeyId : accessKeyId,
   secretAccessKey: secretAccessKey,
 });
 
 
 let upload = multer({
-  storage: multerS3({
+  storage: multers3({
     s3:s3,
-    bucket: process.env.AWS_BUCKET;
+    bucket: process.env.AWS_BUCKET,
     key: function(req,file,cb){
       console.log(file);
       cb(null, file.originalname);
-    }
-  })
-})
+    },
+  }),
+});
 
 imageRouter.post('/image', bodyParser, (req,res,next) => {
   new Image(req.body).save()
@@ -43,12 +44,12 @@ imageRouter.post('/image/:id/new-image', upload.array('upl', 1), (req,res,next) 
     res.send({statusCode: 404, message: 'id not found'});
   }
   Image.findOne({_id: req.params.id})
-  .then( img =>
-    let params = { Key: img.name, Body: img.path};
+  .then( img => {
+    let params = {Bucket: 'ryleeandrews', Key: 'key', Body: img.path};
     s3.putObject(params, function(err,data){
       if(err) console.log(err);
-      else res.send('success');
-    })
-  )
+      else res.send('Sucessful upload');
+    });
+  })
   .catch(err => next(err));
 });
