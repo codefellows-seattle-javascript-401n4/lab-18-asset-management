346ff83a98576b5e9ba09b835fdbac030c3d8bcc
Cole Allen <colesss>
2018-02-08 16:38:34 -0800

    started over with starter code, developer code pending

--- lab-cole/README.md
+++ lab-cole/README.md
@@ -1,7 +1,21 @@
 ![CF](https://camo.githubusercontent.com/70edab54bba80edb7493cad3135e9606781cbb6b/687474703a2f2f692e696d6775722e636f6d2f377635415363382e706e67) 18: Image Uploads w/ AWS S3
-===
+======
 
-## Project Documentation
-* Scaffolding done
-* Demo code almost set up
-* Personal code pending
+## Program Instructions
+Enter in Command Line
+* npm i
+* node index.js
+
+
+## Program Issues
+* ...
+
+
+## Current Program Status
+* started over with starter code, developer code pending
+
+
+## Developer Installs
+* npm init -y
+* npm i -S aws-sdk bcrypt body-parser cors dotenv express fs-extra http-errors jsonwebtoken mongoose morgan multer
+* npm i -D aws-sdk-mock eslint faker jest nodemon superagent 
--- lab-cole/lib/server.js
+++ lab-cole/lib/server.js
@@ -1,37 +1,48 @@
 'use strict';
 
-const cors = require('cors'); //Do I need to install cors and what do I need it for?
-const morgan = require('morgan'); //Do I need to install cors and what do I need it for?
-const express = require('express'); //Do I need to install cors and what do I need it for?
-const mongoose = require('mongoose'); //Do I need to install cors and what do I need it for?
+const cors = require('cors');
+const morgan = require('morgan');
+const express = require('express');
+const mongoose = require('mongoose');
 mongoose.Promise = Promise;
 
 const app = express();
-let server = null //Why?
+let server = null;
 const production = process.env.NODE_ENV === 'production';
 
-//Middleware (do we need these?)
+// global middleware
 app.use(cors({origin: process.env.CORS_ORIGIN}));
 app.use(morgan(production ? 'combined' : 'dev'));
 
-//Routes
-app.use(require('../routes/auth-router.js')); 
-app.use(require('..routes/sample-router.js')); 
-app.use(require('../routes/profile-router.js')); 
+// register routes
+app.use(require('../route/auth-router.js'));
+app.use(require('../route/sample-router.js'));
+app.use(require('../route/profile-router.js'));
 app.all('*', (req, res) => res.sendStatus(404));
-app.use(require('./error-middleware.js'));//Do I need this?
+app.use(require('./error-middleware.js'));
 
 module.exports = {
-    start: () => {
-        app.listen(process.env.PORT || 3000, () => {
-            console.log('server up');
-        });
-        mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/auth_dev', {useMongoClient: true});        
-    },
-    stop: () => {
-        app.close( () => {
-            console.log('server down');
-        });
-        mongoose.disconnect();
-    },
+  start: () => {
+    return new Promise((resolve, reject) => {
+      if(server) 
+        return reject(new Error('__SERVER_ERROR__ server is allready running'));
+      server = app.listen(process.env.PORT, () => {
+        console.log('__SERVER_ON__', process.env.PORT);
+        return resolve();
+      });
+    })
+      .then(() => mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true}));
+  },
+  stop: () => {
+    return new Promise((resolve, reject) => {
+      if(!server) 
+        return reject(new Error('__SERVER_ERROR__ server is allready off'));
+      server.close(() => {
+        server = null;
+        console.log('__SERVER_OFF__');
+        return resolve();
+      });
+    })
+      .then(() => mongoose.disconnect());
+  },
 };
